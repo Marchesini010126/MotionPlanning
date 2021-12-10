6b339cd659496b438ea3a6f53d2463b23f289c62
Evert <evertdevroey@gmail.com>
2021-12-10 16:56:33 +0100

    find outer tangents

--- packages/RobotPlanningRoutines/Dubin.py
+++ packages/RobotPlanningRoutines/Dubin.py
@@ -1,5 +1,3 @@
-import matplotlib.patches
-import pygame as pg
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -18,10 +16,10 @@ def get_circles(x, y, theta, R = 1.):
     x_c2 = x + R * np.cos(theta - np.pi / 2)
     y_c2 = y + R * np.sin(theta - np.pi / 2)
 
-    return (x_c1, y_c1, 'L'), (x_c2, y_c2, 'R'), R
+    return (x_c1, y_c1, 1), (x_c2, y_c2, -1), R
 
 def plot_state(q, R=1., color='r'):
-    c1, c2, R = get_circles(q[0], q[1], q[2]) # c1 is the L-handed circle, c2 the R-handed
+    c1, c2, R = get_circles(q[0], q[1], q[2], R) # c1 is the L-handed circle '1', c2 the R-handed '-1'
 
     # Plot the circles
     plot_circle(*c1[:-1], R, color)
@@ -34,18 +32,25 @@ def plot_state(q, R=1., color='r'):
 
 def get_tangents(circle1, circle2, R):
     # outer tangents
-    p1 = np.array([*circle1[:-1]])
-    p2 = np.array([*circle2[:-1]])
+    if circle1[-1] == circle2[-1]:
 
-    V = p2 - p1
+        p1 = np.array([*circle1[:-1]])
+        p2 = np.array([*circle2[:-1]])
 
-    n = np.array([V[1], -V[0]])
-    n /= np.linalg.norm(n)
+        V = p2 - p1
 
-    t1 = p1 + R * n
-    t2 = p2 + R * n
+        n = np.array([V[1], -V[0]])
+        n /= np.linalg.norm(n)
+        n *= circle1[-1]
 
-    plt.plot([t1[0], t2[0]], [t1[1], t2[1]], '--')
+        t1 = p1 + R * n
+        t2 = p2 + R * n
+
+        plt.plot([t1[0], t2[0]], [t1[1], t2[1]], '--')
+
+    # inner tangents
+    else:
+        pass
 
 #################
 # TESTS
@@ -56,13 +61,17 @@ np.random.seed(8)
 q0 = [np.random.randint(-5,5), np.random.randint(-5,5), np.random.rand(1)*2*np.pi]
 q1 = [np.random.randint(-5,5), np.random.randint(-5,5), np.random.rand(1)*2*np.pi]
 
-plot_state(q0, color='g')
-plot_state(q1, color='r')
+radius = 1.
 
-c0, c1, R = get_circles(q0[0], q0[1], q0[2])
-c2, c3, _    = get_circles(q1[0], q1[1], q1[2])
+plot_state(q0, R=radius, color='g')
+plot_state(q1, R=radius, color='r')
 
-get_tangents(c0,c2,R)
+c0, c1, R = get_circles(q0[0], q0[1], q0[2], R=radius)
+c2, c3, _    = get_circles(q1[0], q1[1], q1[2], R=radius)
+
+for circle1 in [c0, c1]:
+    for circle2 in [c2, c3]:
+        get_tangents(circle1,circle2,R)
 
 plt.gca().set_aspect(1)
 plt.show()
