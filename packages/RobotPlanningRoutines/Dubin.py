4f211eda470b2c1a435f8fe52423dee321fc55dc
Evert <evertdevroey@gmail.com>
2021-12-18 17:26:38 +0100

    implement check for overlapping circles with inner tangents

--- packages/RobotPlanningRoutines/Dubin.py
+++ packages/RobotPlanningRoutines/Dubin.py
@@ -1,10 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-# q = [x, y, theta] with theta measured from global x axis (vertical) and CCW positive
-# R = radius
-# res = samples
-
 class Dubin:
     def __init__(self, q0, q1, R=1., n_samples=None):
         self.radius = R
@@ -17,6 +13,8 @@ class Dubin:
 
         self.circles0 = self.get_circles(self.pos0, self.theta0)
         self.circles1 = self.get_circles(self.pos1, self.theta1)
+
+        self.paths = np.array([])
 
     def get_circles(self, pos, theta):
         cl = pos + self.radius * np.array([np.cos(theta + np.pi / 2), np.sin(theta + np.pi / 2)])
@@ -43,13 +41,13 @@ class Dubin:
         alpha_t = np.arctan2(rt1[1], rt1[0])
         d_alpha = alpha_t - alpha_p # angle from pos to tangent around the circle1 center
         bool = (np.sign(d_alpha) != circle0[-1])  # check if direction of angle and circle are not identical, eg not both CCW
-        d_alpha = circle0[-1] * abs(2 * np.pi * bool - abs(d_alpha))
+        d_alpha = circle0[-1] * abs(2 * np.pi * bool - abs(d_alpha)) # make sure the angle is measured along the direction of the turn
 
         beta_t = np.arctan2(rt2[1], rt2[0])
         beta_p = np.arctan2(rp1[1], rp1[0])
         d_beta = beta_p - beta_t  # angle from tangent to pos around the circle2 center
         bool = (np.sign(d_beta) != circle1[-1])  # check if direction of angle and circle are not identical, eg not both CCW
-        d_beta = circle1[-1] * abs(2 * np.pi * bool - abs(d_beta))
+        d_beta = circle1[-1] * abs(2 * np.pi * bool - abs(d_beta)) # make sure the angle is measured along the direction of the turn
 
         total = abs(d_alpha * self.radius) + L + abs(d_beta * self.radius)
 
@@ -62,24 +60,25 @@ class Dubin:
             for circle1 in self.circles1:
                 path, length = self.get_path(circle0, circle1)
 
-                alpha_samples = abs(self.res * length[0]*self.radius//length[-1])
-                beta_samples = abs(self.res * length[2]*self.radius//length[-1])
-                L_samples = self.res - alpha_samples - beta_samples
+                if type(path[2]) == np.ndarray:
+                    alpha_samples = abs(self.res * length[0]*self.radius//length[-1]).astype(np.int64)
+                    beta_samples = abs(self.res * length[2]*self.radius//length[-1]).astype(np.int64)
+                    L_samples = (self.res - alpha_samples - beta_samples).astype(np.int64)
 
-                n = 0
-                for a in np.linspace(path[0], path[0]+length[0], alpha_samples.astype(int)):
-                    p = circle0[0] + self.radius * np.array([np.cos(a), np.sin(a)])
-                    self.paths[i][n][:-1] += p
-                    n += 1
+                    n = 0
+                    for a in np.linspace(path[0], path[0]+length[0], alpha_samples):
+                        p = circle0[0] + self.radius * np.array([np.cos(a), np.sin(a)])
+                        self.paths[i][n][:-1] += p
+                        n += 1
 
-                for l in np.linspace(path[2], path[3], L_samples.astype(int)):
-                    self.paths[i][n][:-1] = l
-                    n += 1
+                    for l in np.linspace(path[2], path[3], L_samples):
+                        self.paths[i][n][:-1] = l
+                        n += 1
 
-                for b in np.linspace(path[-2], path[-2]+length[-2], beta_samples.astype(int)):
-                    p = circle1[0] + self.radius * np.array([np.cos(b), np.sin(b)])
-                    self.paths[i][n][:-1] += p
-                    n += 1
+                    for b in np.linspace(path[-2], path[-2]+length[-2], beta_samples):
+                        p = circle1[0] + self.radius * np.array([np.cos(b), np.sin(b)])
+                        self.paths[i][n][:-1] += p
+                        n += 1
 
                 i += 1
 
@@ -88,6 +87,10 @@ class Dubin:
     def plot(self):
         plot_state(q0, self.circles0, self.radius, 'g')
         plot_state(q1, self.circles1, self.radius, 'r')
+
+        for path in self.paths:
+            plt.plot(path.T[0], path.T[1], '.')
+
         plt.gca().set_aspect(1)
         plt.show()
 
@@ -134,50 +137,41 @@ def get_tangents(circle1, circle2, R=1.):
     # inner tangents
     else:
         D = np.linalg.norm(V)
-        alpha = np.arccos(R/(0.5*D)) # we know the inner tangents will intersect in the middle
-        alpha *= circle2[-1]
 
-        n = rot2d(V, alpha) # vector pointing from circle center to tangent point
-        n /= np.linalg.norm(n)
+        if D/2 >= R:
+            alpha = np.arccos(R / (0.5 * D))  # we know the inner tangents will intersect in the middle
+            alpha *= circle2[-1]
 
-        t1 = p1 + n * R
-        t2 = p2 - n * R
+            n = rot2d(V, alpha) # vector pointing from circle center to tangent point
+            n /= np.linalg.norm(n)
+
+            t1 = p1 + n * R
+            t2 = p2 - n * R
+
+        else:
+            # the two circles overlap, so no inner tangents can be found:
+            # TODO : create two new circles, both tangent to the two original circles and find tangent points between them
+            # TBD !!!
+
+            print("No inner tangents between overlapping circles!")
+            t1, t2 = np.nan, np.nan
 
     return t1, t2
-
-# def get_length(path):
-#     arc1 = path[0] * path[-1]
-#     arc2 = path[-2] * path[-1]
-#     L = np.linalg.norm(path[2]-path[1])
-#     length = arc1 + arc2 + L
-#     return arc1, L, arc2, length
-#
-# def sample_points(path, length, n_points):
-#     a1_samples = n_samples * (length[0] / length[-1])
-#     a2_samples = n_samples * (length[2] / length[-1])
-#     L_samples = n_samples - a1_samples - a2_samples
-#
-#     points = np.zeros(n_samples, 2)
-
-
 
 
 #################
 # TESTS
 #################
 
-np.random.seed()
+np.random.seed(40) # a nice seed is 9, seed for linspace debug eg.40
 
 q0 = [np.random.randint(-5,5), np.random.randint(-5,5), np.random.random_sample()*2*np.pi]
 q1 = [np.random.randint(-5,5), np.random.randint(-5,5), np.random.random_sample()*2*np.pi]
-
 radius = 1.5
 
-d = Dubin(q0, q1, radius, 50)
-paths = d.make_path()
-print(paths)
+d = Dubin(q0, q1, radius, 40)
 
-plt.plot(paths[3].T[0], paths[3].T[1], '.')
+paths = d.make_path()
 d.plot()
 
-plt.show()
+
