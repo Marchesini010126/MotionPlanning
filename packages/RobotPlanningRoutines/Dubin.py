686f4782333816438ff4d27ee1bed39fa0a43a16
Evert <evertdevroey@gmail.com>
2022-01-02 16:11:52 +0100

    implement check for goal inside of turning circle

--- packages/RobotPlanningRoutines/Dubin.py
+++ packages/RobotPlanningRoutines/Dubin.py
@@ -3,40 +3,6 @@ from matplotlib import pyplot as plt
 
 class Dubin:
 
-    """
-    Dubin creates a Dubin's path between states q0 and q1, keeping to the minimum turning radius R, divided in n_samples
-    that can be used for collision checks.
-
-    INPUT:
-        q0: [x0, y0, theta0]    initial state, theta measured CCW positive from global x-axis
-        q1: [x1, y1, theta1]    final state
-
-    FUNCTIONS:
-        get_circles(position, orientation)
-            finds the turn left and turn right circles for a given position and orientation
-            Output: two cirlces of type:
-                tuple: (np.ndarray: center_pos, int: turning_sign)
-
-        get_path(circle0, circle1)
-            finds a single path between two given circles adn their signs
-            Output:
-                path of type:
-                    list: [float: angle to pos0, float: angle to tangent0, ndarray: pos of tangent 0, ndarray: pos of tangent 1, float: angle to tangent1, float: angle to pos1]
-                length of type:
-                    list: [float: angle on circle0, float: distance between tangents on the two cirlces, float: angle on circle 1, float: total distance]
-
-        make_path()
-            finds all paths between two states
-            Output:
-                PATHS of type:
-                    ndarray of shape (num_paths, num_samples, 3):
-                        number of paths found (4) sampled at n_samples points, containing the configurations [x, y, theta]
-
-        plot()
-            plots q0 and q1 states as two vectors (green for inital, red for final) with the turning circles and their signs,
-            if make_path() has been called, the paths are plotted as well, showing up as '.' at each sampled point.
-    """
-
     def __init__(self, q0, goal, R=1., n_samples=None):
         self.radius = R
         self.pos0 = np.array(q0[:2])
@@ -86,23 +52,24 @@ class Dubin:
             path, length = self.get_path(circle, self.pos1)
             lengths = np.append(lengths, length[-1])
 
-            alpha_samples = abs(self.res * length[0] * self.radius // length[-1]).astype(np.int64)
-            L_samples = (self.res - alpha_samples).astype(np.int64)
+            if type(path[2]) == np.ndarray:
+                alpha_samples = abs(self.res * length[0] * self.radius // length[-1]).astype(np.int64)
+                L_samples = (self.res - alpha_samples).astype(np.int64)
 
-            n = 0
-            for a in np.linspace(path[0], path[0] + length[0], alpha_samples):
-                p = circle[0] + self.radius * np.array([np.cos(a), np.sin(a)])
-                th = a + circle[-1] * np.pi/2
-                self.paths[i][n][:-1] += p
-                self.paths[i][n][-1] += th
-                n += 1
+                n = 0
+                for a in np.linspace(path[0], path[0] + length[0], alpha_samples):
+                    p = circle[0] + self.radius * np.array([np.cos(a), np.sin(a)])
+                    th = a + circle[-1] * np.pi/2
+                    self.paths[i][n][:-1] += p
+                    self.paths[i][n][-1] += th
+                    n += 1
 
-            V = self.pos1 - path[2]
-            th = np.arctan2(V[1], V[0])
-            for l in np.linspace(path[2], self.pos1, L_samples):
-                self.paths[i][n][:-1] = l
-                self.paths[i][n][-1] += th
-                n += 1
+                V = self.pos1 - path[2]
+                th = np.arctan2(V[1], V[0])
+                for l in np.linspace(path[2], self.pos1, L_samples):
+                    self.paths[i][n][:-1] = l
+                    self.paths[i][n][-1] += th
+                    n += 1
 
             i += 1
 
@@ -113,16 +80,13 @@ class Dubin:
         return self.paths
 
     def plot(self):
-        plot_state(q0, self.circles, self.radius, 'g')
 
-        for path in self.paths:
+        for [path, color] in zip(self.paths, ['lightblue', 'navajowhite']):
+            circles = self.get_circles(path[-1][:2], path[-1][-1])
+            plot_state(path[-1], circles, self.radius, color=color)
             plt.plot(path.T[0], path.T[1], '.')
 
-        circles = self.get_circles(self.paths[0][-1][:2], self.paths[0][-1][-1])
-        plot_state(self.paths[0][-1], circles, self.radius, color='lightblue')
-
-        circles = self.get_circles(self.paths[1][-1][:2], self.paths[1][-1][-1])
-        plot_state(self.paths[1][-1], circles, self.radius, color='navajowhite')
+        plot_state(q0, self.circles, self.radius, 'g')
 
         plt.plot(self.pos1[0], self.pos1[1], 'x', color='r')
 
@@ -154,15 +118,19 @@ def tangent(circle, point, R=1.):
     V = point - circle[0]
     D = np.linalg.norm(V)
 
-    alpha = np.arccos(R/D)
-    alpha *= -circle[-1]
+    if D > R:
+        alpha = np.arccos(R/D)
+        alpha *= -circle[-1]
 
-    n = rot2d(V, alpha)  # vector pointing from circle center to tangent point
-    n /= np.linalg.norm(n)
+        n = rot2d(V, alpha)  # vector pointing from circle center to tangent point
+        n /= np.linalg.norm(n)
 
-    tangent = circle[0] + R * n
+        tangent = circle[0] + R * n
 
-    return tangent
+        return tangent
+
+    else:
+        return np.inf
 
 
 
@@ -171,16 +139,15 @@ def tangent(circle, point, R=1.):
 #################
 
 
-np.random.seed() # a nice seed is 9, seed for overlapping debug eg.40
+np.random.seed()
 
 q0 = [np.random.randint(-5,5), np.random.randint(-5,5), np.random.random_sample()*2*np.pi]
-
 point = [np.random.randint(-5,5), np.random.randint(-5,5)]
 
 # q0 = [0, 0, 0]
-# q1 = [0, 2.5, 0]
+# point = [0,-2]
 
-radius = 1.
+radius = 1.5
 
 d = Dubin(q0, point, radius, 25)
 
